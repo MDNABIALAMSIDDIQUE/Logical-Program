4ad4617b7b812a48328e8dcfd82ac58ab75375fd
MD NABI ALAM <nabi.alam@finagg.in>
2024-10-08 11:23:41 +0530

    sum of all numbers from 1 to 50 Without loop

---
+++ Core_Java_Logical_Program/src/core/com/SumofNumberNth_Without_Loop.java
@@ -0,0 +1,10 @@
+package core.com;
+//Write a for loop to calculate the sum of all numbers from 1 to 50
+public class SumofNumberNth_Without_Loop {
+	
+	public static void main(String[] args) {
+		int num =50;
+		int sum = (num*(num+1))/2;
+		System.out.println("Sum of Totel Number -> "+sum);
+	}
+}
